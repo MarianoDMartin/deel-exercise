7a83bfa3572bd0e63e3b33285abc840b31c04815
Mariano Martin <mariano.d.martin@gmail.com>
2022-11-04 08:24:04 -0300

    fix get contract endpoint to return only profile related contracts

--- src/controllers/contracts.js
+++ src/controllers/contracts.js
@@ -1,8 +1,16 @@
 async function get (req, res) {
   const { Contract } = req.app.get('models')
   const { id } = req.params
+
+  if (!id) return res.status(400).end()
+
   const contract = await Contract.findOne({ where: { id } })
   if (!contract) return res.status(404).end()
+  if (contract.ContractorId !== req.profile.id && contract.ClientId !== req.profile.id) {
+    console.log('here')
+    return res.status(403).end()
+  }
+
   res.json(contract)
 }
 
